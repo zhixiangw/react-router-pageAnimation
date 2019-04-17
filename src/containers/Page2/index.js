a78c5a77cacac8d2645fecd56a07fec39a95d2e5
zhixiang_w <zhixiang_w@sina.cn>
2019-04-17 20:37:04 +0800

    当前url进行props replace或者window replace的异常情况处理

--- src/containers/Page2/index.js
+++ src/containers/Page2/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export default class Page2 extends React.Component {
+  componentDidMount() {
+    // this.props.history.replace('/page2?query=props1')
+    // this.props.history.replace('/page2?query=props2')
+
+    // window.location.replace(window.location.origin + '/#/page2?query=window1')
+    // window.location.replace(window.location.origin + '/#/page2?query=window2')
+    // window.location.replace(window.location.origin + '/#/page2?query=window3')
+  }
   render() {
     return (
       <div className="page page2">
@@ -9,6 +17,12 @@ export default class Page2 extends React.Component {
         <button onClick={() => this.props.history.replace('/page3')}>props replace to Page3</button>
         <button onClick={() => window.location.replace(window.location.origin + '/#/page3')}>window replace to Page3</button>
         <button onClick={() => window.location.href = window.location.origin + '/#/page3'}>window href to Page3</button>
+        <button onClick={() => {
+          this.props.history.replace('/page2?query=props3')
+          setTimeout(() => {
+            window.location.href = window.location.origin + '/#/page3'
+          }, 300);
+        }}>props replace then window href to Page3</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -11,46 +11,64 @@ import Page3 from 'containers/Page3'
 import Page4 from 'containers/Page4'
 
 // 一个不准确的 appHistoryStack，不对外暴露接口，不能当做历史记录参考
-const appHistoryStack = sessionStorage.getItem('appHistoryStack') && JSON.parse(sessionStorage.getItem('appHistoryStack')) || [window.location.href]
+const setStorage = false
+const appHistoryStack = setStorage && sessionStorage.getItem('appHistoryStack') && JSON.parse(sessionStorage.getItem('appHistoryStack')) || [window.location.href]
 const replaceState = history.replaceState
 const pushState = history.pushState
 let appAction = 'FORWARD'
+let onAnimation = false
+let animationClassName = ''
 
 history.replaceState = function() {
-  setTimeout(() => appHistoryStack.splice(-1, 1, window.location.href), 0)
+  setTimeout(() => appHistoryStack.splice(-1, 1, window.location.href.split('?')[0]), 0)
   replaceState.apply(history, arguments)
 }
 history.pushState = function() {
-  setTimeout(() => appHistoryStack.push(window.location.href), 0)
+  setTimeout(() => appHistoryStack.push(window.location.href.split('?')[0]), 0)
   pushState.apply(history, arguments)
 }
 
 window.addEventListener('hashchange', (HashChangeEvent) => {
   const { newURL, oldURL } = HashChangeEvent
-  const newURLIndex = appHistoryStack.indexOf(newURL)
-  const oldURLIndex = appHistoryStack.indexOf(oldURL)
-  if (newURLIndex === -1) {
-    appHistoryStack.push(newURL)
-  }
-  if (newURLIndex === -1 || newURLIndex - oldURLIndex > 0) {
-    appAction = 'FORWARD'
+  const newURLHash = newURL.split('?')[0]
+  const oldURLHash = oldURL.split('?')[0]
+  if (newURLHash !== oldURLHash) {
+    const newURLIndex = appHistoryStack.indexOf(newURL)
+    const oldURLIndex = appHistoryStack.indexOf(oldURL)
+    if (newURLIndex === -1) {
+      appHistoryStack.push(newURL)
+    }
+    if (newURLIndex === -1 || newURLIndex - oldURLIndex > 0) {
+      appAction = 'FORWARD'
+    } else {
+      appAction = 'GOBACK'
+    }
   } else {
-    appAction = 'GOBACK'
+    appHistoryStack.splice(appHistoryStack.indexOf(newURL), 1, newURL)
   }
 })
 
 ReactDOM.render((
   <HashRouter history={createBrowserHistory()}>
     <Route render={({ location, history }) => {
-      let animationClassName = ''
       if (['PUSH', 'REPLACE'].includes(history.action)) {
-        animationClassName = 'slide-left'
+        animationClassName = onAnimation && animationClassName ? animationClassName : 'slide-left'
       } else {
         animationClassName = appAction === 'FORWARD' ? 'slide-left' : 'slide-right'
       }
       return (
         <TransitionGroup className={animationClassName}>
-          <CSSTransition key={location.pathname} onEntered={() => sessionStorage.setItem('appHistoryStack', JSON.stringify(appHistoryStack))} classNames="animation" timeout={304}>
+          <CSSTransition
+            key={location.pathname}
+            classNames="animation"
+            timeout={304}
+            onEnter={() => {
+              onAnimation = true
+            }}
+            onEntered={() => {
+              onAnimation = false
+              sessionStorage.setItem('appHistoryStack', JSON.stringify(appHistoryStack))
+            }} >
             <Switch location={location}>
               <Route exact path="/page1" component={Page1}/>
               <Route exact path="/page2" component={Page2}/>
